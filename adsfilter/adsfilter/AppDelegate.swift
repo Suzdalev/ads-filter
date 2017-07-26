f023f9109d98385d4fc5de13c816b322ebf58622
Ihor Suzdaliev <swindows101@gmail.com>
2017-07-26 13:32:34 +0300

    initial commit

---
+++ adsfilter/adsfilter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  adsfilter
+//
+//  Created by Igor Suzdalev on 26.07.17.
+//  Copyright © 2017 Igor Suzdalev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ adsfilter/adsfilter/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  adsfilter
+//
+//  Created by Igor Suzdalev on 26.07.17.
+//  Copyright © 2017 Igor Suzdalev. All rights reserved.
+//
+
+import Cocoa
+import Alamofire
+
+class ViewController: NSViewController {
+
+    @IBOutlet weak var textSourceLinks: NSScrollView!
+    @IBOutlet weak var textFilteredLinks: NSScrollView!
+    @IBAction func btnMagic(_ sender: Any) {
+        
+        //btn code
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
